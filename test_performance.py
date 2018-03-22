0368d401fd6aa8946b4fb4db00545a612bc80897
USER <user@AL00182184.local>
2018-03-22 10:49:26 +0900

    default params test_perf

--- test_performance.py
+++ test_performance.py
@@ -8,7 +8,7 @@ from dropout import ContiguousDropout
 import argparse
 # python -m cProfile -s cumtime test_performance.py --repeats  10
 parser = argparse.ArgumentParser(description='')
-parser.add_argument('--repeats', type=int, default=10000, metavar='N')
+parser.add_argument('--repeats', type=int, default=100000, metavar='N')
 parser.add_argument('--n-features', type=int, default=500, metavar='N')
 parser.add_argument('--batch-size', type=int, default=1000, metavar='N')
 parser.add_argument('--no-cuda', action='store_true', default=False,help='disables CUDA training')
@@ -19,8 +19,6 @@ print('GPU: ',args.cuda)
 print(torch.__version__)
 if args.cuda:
     torch.set_default_tensor_type('torch.cuda.FloatTensor')
-
-n = 1000
 
 import time, math
 def time_since(since):
@@ -33,22 +31,23 @@ def test_ContiguousDropout():
     n = args.batch_size
     k = args.n_features
     dropout = ContiguousDropout()
-    x = Variable(torch.ones(n,k))
+    y = Variable(torch.ones(n,k))
     for _ in range(args.repeats):        
-        y = dropout(x)
+        y = dropout(y)
     return None
 
 def test_Dropout():
     n = args.batch_size
     k = args.n_features
     dropout = nn.Dropout()
-    x = Variable(torch.ones(n,k))
+    y = Variable(torch.ones(n,k))
     for _ in range(args.repeats):        
-        y = dropout(x)
+        y = dropout(y)
     return None
 
 
-start = time.time();test_ContiguousDropout();print(time_since(start),' ContiguousDropout')
-start = time.time();test_Dropout();print(time_since(start),' Dropout')
-start = time.time();test_ContiguousDropout();print(time_since(start),' ContiguousDropout')
-start = time.time();test_Dropout();print(time_since(start),' Dropout')
+for _ in range(3):
+    start = time.time();test_ContiguousDropout();print(time_since(start),' ContiguousDropout')
+    start = time.time();test_Dropout();print(time_since(start),' Dropout')
+    start = time.time();test_ContiguousDropout();print(time_since(start),' ContiguousDropout')
+    start = time.time();test_Dropout();print(time_since(start),' Dropout')
