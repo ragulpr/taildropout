384bbb4cc2e71a78afa4f3bd27d5d291a68201e1
USER <user@AL00182184.local>
2018-03-27 21:38:34 +0900

    p=0 p=1 test coverage

--- test.py
+++ test.py
@@ -91,7 +91,7 @@ def test_dropoutprob():
             epsilon = 5e-2
         x = Variable(torch.ones(n, k))
 
-        for p in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]:
+        for p in [0,0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9,1.]:
             dropout = ContiguousDropout(p)
             y = dropout(x)
             err = (1 - y.mean() - p).abs()
